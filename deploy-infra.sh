8f3eb6253d90077f9767f18d488568817a5357e8
thaohoangc3 <thaominhhoangvn@outlook.com>
2022-11-20 00:28:10 +0700

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,22 @@ REGION=ap-southeast-1
 CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query Account --output text`
+CODEPIPELINE_BUCKET=$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID
+
+# Deploy static resources
+echo -e "\n\n=================Deploying static resources=================\n\n"
+
+aws cloudformation deploy \
+    --template-file setup.yml \
+    --stack-name $STACK_NAME-setup \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --no-fail-on-empty-changeset \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --parameter-overrides \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 # Deploy the CloudFormation template
 
